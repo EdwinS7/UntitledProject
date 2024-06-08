647cf1804dcc662680da66c3a511045ac84f1c11
Edwn <88218388+EdwinS7@users.noreply.github.com>
2024-06-08 04:21:10 -0400

    HTTP hotfix & Webhook example

--- Untitled Renderer/Include/Backend/Wrapper/Features.hpp
+++ Untitled Renderer/Include/Backend/Wrapper/Features.hpp
@@ -215,22 +215,25 @@ namespace Math {
 };
 
 namespace Http {
+    size_t WriteCallback( void* contents, size_t size, size_t nmemb, std::string* s ) {
+        s->append( ( char* ) contents, size * nmemb );
+        return size * nmemb;
+    }
+
     std::string Get( const std::string& url ) {
         CURL* Curl;
         std::string Response;
 
         Curl = curl_easy_init( );
         if ( Curl ) {
-            curl_easy_setopt( Curl, CURLOPT_URL, url );
+            curl_easy_setopt( Curl, CURLOPT_URL, url.c_str( ) );
             curl_easy_setopt( Curl, CURLOPT_WRITEDATA, &Response );
-            curl_easy_setopt( Curl, CURLOPT_WRITEFUNCTION, +[ ] ( void* contents, size_t size, size_t nmemb, std::string* s ) -> size_t {
-                s->append( ( char* ) contents, size * nmemb );
-                return size * nmemb;
-            } );
-            
-            auto Result = curl_easy_perform( Curl );
-            if ( Result != CURLE_OK )
-                std::cout << curl_easy_strerror( Result ) << "\n";
+            curl_easy_setopt( Curl, CURLOPT_WRITEFUNCTION, WriteCallback );
+
+            CURLcode Result = curl_easy_perform( Curl );
+            if ( Result != CURLE_OK ) {
+                std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror( Result ) << std::endl;
+            }
 
             curl_easy_cleanup( Curl );
         }
@@ -245,18 +248,21 @@ namespace Http {
         Curl = curl_easy_init( );
         if ( Curl ) {
             curl_easy_setopt( Curl, CURLOPT_POST, 1L );
-            curl_easy_setopt( Curl, CURLOPT_URL, url );
+            curl_easy_setopt( Curl, CURLOPT_URL, url.c_str( ) );
             curl_easy_setopt( Curl, CURLOPT_WRITEDATA, &Response );
-            curl_easy_setopt( Curl, CURLOPT_POSTFIELDS, post_data );
-            curl_easy_setopt( Curl, CURLOPT_WRITEFUNCTION, +[ ] ( void* contents, size_t size, size_t nmemb, std::string* s ) -> size_t {
-                s->append( ( char* ) contents, size * nmemb );
-                return size * nmemb;
-            } );
+            curl_easy_setopt( Curl, CURLOPT_POSTFIELDS, post_data.c_str( ) );
+            curl_easy_setopt( Curl, CURLOPT_WRITEFUNCTION, WriteCallback );
+
+            struct curl_slist* headers = NULL;
+            headers = curl_slist_append( headers, "Content-Type: application/json" );
+            curl_easy_setopt( Curl, CURLOPT_HTTPHEADER, headers );
 
             CURLcode Result = curl_easy_perform( Curl );
-            if ( Result != CURLE_OK )
-                std::cout << curl_easy_strerror( Result ) << "\n";
+            if ( Result != CURLE_OK ) {
+                std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror( Result ) << std::endl;
+            }
 
+            curl_slist_free_all( headers );
             curl_easy_cleanup( Curl );
         }
 
@@ -273,15 +279,18 @@ namespace Http {
             curl_easy_setopt( Curl, CURLOPT_URL, url.c_str( ) );
             curl_easy_setopt( Curl, CURLOPT_WRITEDATA, &Response );
             curl_easy_setopt( Curl, CURLOPT_POSTFIELDS, put_data.c_str( ) );
-            curl_easy_setopt( Curl, CURLOPT_WRITEFUNCTION, +[ ] ( void* contents, size_t size, size_t nmemb, std::string* s ) -> size_t {
-                s->append( ( char* ) contents, size * nmemb );
-                return size * nmemb;
-            } );
+            curl_easy_setopt( Curl, CURLOPT_WRITEFUNCTION, WriteCallback );
+
+            struct curl_slist* headers = NULL;
+            headers = curl_slist_append( headers, "Content-Type: application/json" );
+            curl_easy_setopt( Curl, CURLOPT_HTTPHEADER, headers );
 
             CURLcode Result = curl_easy_perform( Curl );
-            if ( Result != CURLE_OK )
-                std::cerr << curl_easy_strerror( Result ) << "\n";
+            if ( Result != CURLE_OK ) {
+                std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror( Result ) << std::endl;
+            }
 
+            curl_slist_free_all( headers );
             curl_easy_cleanup( Curl );
         }
 
@@ -297,14 +306,12 @@ namespace Http {
             curl_easy_setopt( Curl, CURLOPT_CUSTOMREQUEST, "DELETE" );
             curl_easy_setopt( Curl, CURLOPT_URL, url.c_str( ) );
             curl_easy_setopt( Curl, CURLOPT_WRITEDATA, &Response );
-            curl_easy_setopt( Curl, CURLOPT_WRITEFUNCTION, +[ ] ( void* contents, size_t size, size_t nmemb, std::string* s ) -> size_t {
-                s->append( ( char* ) contents, size * nmemb );
-                return size * nmemb;
-            } );
+            curl_easy_setopt( Curl, CURLOPT_WRITEFUNCTION, WriteCallback );
 
             CURLcode Result = curl_easy_perform( Curl );
-            if ( Result != CURLE_OK )
-                std::cerr << curl_easy_strerror( Result ) << "\n";
+            if ( Result != CURLE_OK ) {
+                std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror( Result ) << std::endl;
+            }
 
             curl_easy_cleanup( Curl );
         }
