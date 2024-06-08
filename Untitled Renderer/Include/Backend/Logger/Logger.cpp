0f96f478c5e1c57b68722536cb20d1486e79cabd
Edwn <88218388+EdwinS7@users.noreply.github.com>
2024-06-08 17:53:25 -0400

    Colored Logs

--- Untitled Renderer/Include/Backend/Logger/Logger.cpp
+++ Untitled Renderer/Include/Backend/Logger/Logger.cpp
@@ -1,23 +1,30 @@
 #include "Logger.hpp"
 
 void cLogger::Print( LogLevel level, const std::string& message ) {
-	std::string fmt_message = "";
+    std::string fmt_message;
+    DWORD color = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE;
 
-	switch ( level ) {
-	case LogLevel::Normal:
-		fmt_message.append( "[+] " );
-		break;
-	case LogLevel::Information:
-		fmt_message.append( "[?] " );
-		break;
-	case LogLevel::Warning:
-		fmt_message.append( "[!] " );
-		break;
-	case LogLevel::Error:
-		fmt_message.append( "[x] " );
-		break;
-	}
+    switch ( level ) {
+    case LogLevel::Normal:
+        fmt_message.append( "[+] " );
+        break;
+    case LogLevel::Information:
+        color = FOREGROUND_GREEN | FOREGROUND_BLUE;
+        fmt_message.append( "[?] " );
+        break;
+    case LogLevel::Warning:
+        color = FOREGROUND_RED | FOREGROUND_GREEN;
+        fmt_message.append( "[!] " );
+        break;
+    case LogLevel::Error:
+        color = FOREGROUND_RED;
+        fmt_message.append( "[x] " );
+        break;
+    }
 
-	fmt_message.append( message );
-	std::cout << fmt_message << "\n";
+    HANDLE hStdOut = GetStdHandle( STD_OUTPUT_HANDLE );
+    SetConsoleTextAttribute( hStdOut, color );
+    
+    fmt_message.append( message );
+    std::cout << fmt_message << "\n";
 }
