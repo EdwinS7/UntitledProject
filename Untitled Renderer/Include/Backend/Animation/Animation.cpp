5e3c7deed5944b55db14e53924193e98d85b9ce9
Edwn <88218388+EdwinS7@users.noreply.github.com>
2024-06-09 01:57:06 -0400

    PI

--- Untitled Renderer/Include/Backend/Animation/Animation.cpp
+++ Untitled Renderer/Include/Backend/Animation/Animation.cpp
@@ -5,7 +5,7 @@ float cAnimations::Lerp( float from, float to, float delta_time ) {
 }
 
 float cAnimations::Coserp( float from, float to, float delta_time ) {
-    float ft = delta_time * 3.1415927f;
+    float ft = delta_time * M_PI;
     float f = ( 1 - std::cos( ft ) ) * 0.5f;
     return from * ( 1 - f ) + to * f;
 }
