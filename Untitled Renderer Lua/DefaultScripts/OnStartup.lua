99d19ae4b454f2c50b980011bf23ce704d88fbd8
Edwn <88218388+EdwinS7@users.noreply.github.com>
2024-07-21 22:26:26 -0400

    Default Scripts

---
+++ Untitled Renderer Lua/DefaultScripts/OnStartup.lua
@@ -0,0 +1,34 @@
+print("Startup time: " .. Client.GetRealtime())
+print("Getting fonts from registry")
+
+local Fonts = Client.GetUsableFonts()
+
+for i, v in pairs(Fonts) do
+    print(v)
+end
+
+function F11Fullscreen()
+    if Input.IsKeyPressed(Enums.Keys.F11) then
+        Window.SetFullscreen(not Window.GetFullscreen())
+    end
+end
+
+function Paint()
+    -- Framerate Counter (Top-Right)
+    local TextSize = Renderer.GetTextSize(Renderer.GetDefaultFont(), tostring(Client.GetFramerate()))
+    local ScreenSize = Renderer.GetScreenSize()
+
+    Renderer.FilledRectangle(Vector2.new((ScreenSize.x - TextSize.x) - 9, 5), Vector2.new(TextSize.x + 4, TextSize.y), Color.new(0, 0, 0, 255), 0, Enums.CornerFlags.None)
+    Renderer.Text(Renderer.GetDefaultFont(), tostring(Client.GetFramerate()), Vector2.new(ScreenSize.x - 8 - TextSize.x, 5), Color.new(255, 255, 255, 255))
+
+    -- Logs (Top-Left)
+    local ConsoleLogs = Client.GetLogs(Enums.LogLevel.All)
+    local String = table.concat(ConsoleLogs, "\n")
+
+    Renderer.Text(Renderer.GetDefaultFont(), String, Vector2.new(5, 5), Color.new(0, 0, 0, 255))
+end
+
+Connect("OnInputUpdate", F11Fullscreen)
+Connect("OnInterfacePaint", Paint)
+
+print("Framerate is only extemely low because of Renderer.Text, its rendering each letter with a texture at the moment!")
